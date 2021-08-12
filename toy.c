566995db6d96f893921f9d66c198e9f25d75bfd2
Anay Wadhera <anay1018@gmail.com>
2021-08-12 20:40:22 +0000

    print information about the elf runtime file

--- toy.c
+++ toy.c
@@ -1,6 +1,12 @@
 #include "elf.h"
 
-int hello(int i) {
-    printf("runtime addr: %x", i);
+int hello(void* i) {
+    printf("runtime addr: %x\n", i);
+    int sections = elf_getNumSections(i);
+    printf("ELF # of sections: %d\n", sections);
+    for (int cnt = 0; cnt < sections; cnt++) {
+        printf("ELF section information: %s\n", elf_getSectionName(i, cnt));
+    }
+    printf("Eyrie runtime entry point: %x\n", elf_getEntryPoint(i));
     return 10;
 }
